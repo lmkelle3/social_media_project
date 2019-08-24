fb5c8b51d6330f51a1c7bde25513ed8a8893e057
Lindsay Kelley <lmkelle3@Lindsays-MacBook-Air.local>
2019-08-24 11:52:44 -0700

    added switch for location cases

--- src/components/dashboard/StatusList.js
+++ src/components/dashboard/StatusList.js
@@ -5,6 +5,7 @@ import { withRouter } from "react-router-dom";
 import { ListGroup } from "reactstrap";
 
 const StatusList = props => {
+  console.log("PROPS:", props);
   if (props.statuses) {
     console.log("P.STATUS:", props.statuses);
     let listOfStatuses = props.statuses.map(status => (
@@ -17,11 +18,28 @@ const StatusList = props => {
 };
 
 const mapStateToProps = (state, props) => {
+  let currentPage = props.location.pathname;
+  console.log("Current:", currentPage);
+  let theStatuses = [];
+
+  switch (currentPage) {
+    case "/homepage":
+      // return;
+      theStatuses = state.statuses.filter(
+        status => status.user_id == props.match.params.user_id
+      );
+      break;
+    case `/profile/${props.match.params.user_id}`:
+      // return;
+      theStatuses = state.statuses.filter(
+        status => status.userId == props.match.params.user_id
+      );
+      break;
+    default:
+      return;
+  }
   return {
-    statuses: state.statuses.filter(status => {
-      console.log("STATUS YO:", status);
-      return status.userId == props.match.params.user_id;
-    })
+    statuses: theStatuses
   };
 };
 export default withRouter(connect(mapStateToProps)(StatusList));
